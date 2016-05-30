2a648241d685fe63a49cbf1031ac39872bd6bdde
Jeff Hutchison <jeff@jeffhutchison.com>
2016-05-29 21:19:22 -0400

    Implement index generation.

---
+++ index.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+
+	"jhhgo.us/tmp/markdown"
+)
+
+var indexTmpl = template.Must(template.ParseFiles(filepath.Join("templates", "index.html")))
+
+// IndexEntry is an reference to a document.
+type IndexEntry struct {
+	Path string
+	markdown.Meta
+}
+
+// Index holds the metadata for all content.
+type Index []IndexEntry
+
+// NewIndex loads the index.
+func NewIndex(path string) (Index, error) {
+	files, err := filepath.Glob(filepath.Join(path, "*.md"))
+	if err != nil {
+		return nil, err
+	}
+
+	idx := Index{}
+	for _, f := range files {
+		doc, err := markdown.NewDocument(f)
+		if err != nil {
+			return nil, err
+		}
+		idx = append(idx, IndexEntry{Path: "/" + f, Meta: doc.Meta})
+	}
+	return idx, nil
+}
+
+// IndexHandler displays the page index.
+func (idx Index) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	err := indexTmpl.Execute(w, idx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
---
+++ testdata/ok_complete.md
@@ -0,0 +1,11 @@
+---
+title: Complete
+date: 2016-05-27T23:45:46.000Z
+categories:
+  - test
+tags:
+  - foo
+  - bar
+---
+
+# Complete
