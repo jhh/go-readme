47401005877ea866e8d2e15f5b12016e87c7ecac
Jeff Hutchison <jeff@jeffhutchison.com>
2016-06-01 17:31:09 -0400

    Group index by categories.

--- content_test.go
+++ content_test.go
@@ -17,8 +17,8 @@ func TestContentHandler(t *testing.T) {
 
 	// override validPath set in readme.go
 	saveValidPath := validPath
-	validPath = regexp.MustCompile("^/(testdata)/(ok_complete\\.md)$")
-	res, err := http.Get(ts.URL + "/testdata/ok_complete.md")
+	validPath = regexp.MustCompile("^/(testdata)/(ok_cat_bar1\\.md)$")
+	res, err := http.Get(ts.URL + "/testdata/ok_cat_bar1.md")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -30,11 +30,11 @@ func TestContentHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := "<title>Complete</title>"
+	want := "<title>Category bar: 2016-04-27T23:45:46.000Z</title>"
 	if !strings.Contains(string(body), want) {
 		t.Errorf("body does not contain %q, got:\n%q\n", want, body)
 	}
-	want = "<h1>Complete</h1>"
+	want = "<h1>Category bar: 2016-04-27T23:45:46.000Z</h1>"
 	if !strings.Contains(string(body), want) {
 		t.Errorf("body does not contain %q, got:\n%q\n", want, body)
 	}
---
+++ testdata/ok_cat_foo1.md
@@ -0,0 +1,11 @@
+---
+title: 'Category foo: 2016-05-27T23:45:46.000Z'
+date: 2016-05-27T23:45:46.000Z
+categories:
+  - foo
+tags:
+  - foo
+  - bar
+---
+
+# Category foo: 2016-05-27T23:45:46.000Z
